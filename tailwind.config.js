fba54d852ac9f52b2100a8f15d250430a7f26aa7
Mohammed Dawoud <43849138+MohammedDawoud@users.noreply.github.com>
2024-11-09 17:38:40 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        base: "#0a6dca",
+        second: "#0a6dca",
+        light: "#0a6dca",
+        white: "#fff"
+      },
+    },
+  },
+  plugins: [],
+};
